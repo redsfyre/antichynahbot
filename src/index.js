22abb734bb2a3d739a0b8bc28c6ee6529f5b2ce7
Yasin İsa YILDIRIM <37442657+redsfyre@users.noreply.github.com>
2020-05-26 00:50:20 +0300

    Update index.js
    Aptal aptal hatalar yapmışım, düzelt düzelt bitmiyor. Kullandığım modüllere bakıp sıfırdan yazmanız hayrınıza olur :P

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = 'process.env.BOT_TOKEN';
+const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, {polling: true});
 const containsChinese = require('contains-chinese');
 const express = require('express');
